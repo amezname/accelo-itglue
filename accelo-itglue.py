d7b56047e9bce570c3e9e1f418e478098c9ce8f0
amezname <77634962+amezname@users.noreply.github.com>
2021-11-12 11:11:34 -0500

    Update accelo-itglue.py

--- accelo-itglue.py
+++ accelo-itglue.py
@@ -3,24 +3,13 @@ from requests.exceptions import HTTPError
 import config as cfg
 
 
-#Authentication and Token Creation
-
-url = f"https://{cfg.deployment}/oauth2/v0/token?grant_type=client_credentials&scope=read(all)&expires_in=3600"
-
-payload={}
-headers = {
-	'Content-Transfer-Encoding': 'application/x-www-form-urlencoded',
-	'Authorization': f'Basic {cfg.acc_credentials}'
-}
-
-auth = requests.request("POST", url, headers=headers, data=payload).json()
-token = auth['access_token']
-
 itg_headers = {
-  		'x-api-key': cfg.api_key
+  		'x-api-key': cfg.api_key,
+  		'Content-Type' : 'application/vnd.api+json'
   	}
 
 #Functions
+
 def logger(message):
 	file = cfg.filepath
 	now = datetime.datetime.now()
@@ -28,6 +17,21 @@ def logger(message):
 	with open(file, "a") as log:
 		log.write(logtime + "   " + message + "\n")
 
+# Accelo Authentication
+def auth():
+	url = f"https://{cfg.deployment}/oauth2/v0/token?grant_type=client_credentials&scope=read(all)&expires_in=3600"
+
+	payload={}
+	headers = {
+		'Content-Transfer-Encoding': 'application/x-www-form-urlencoded',
+		'Authorization': f'Basic {cfg.acc_credentials}'
+	}
+
+	auth = requests.request("POST", url, headers=headers, data=payload).json()
+	token = auth['access_token']
+	return token
+
+# Accelo GET requests
 def get_acc(token, url):
 
 	payload = {}
@@ -46,7 +50,7 @@ def get_acc(token, url):
 		r = response.json()
 		return r
 	
-
+# ITGlue GET requests
 def get_itg(url,headers):
 
 	payload={}
@@ -62,7 +66,7 @@ def get_itg(url,headers):
 		r = response.json()
 		return r
 	
-
+# ITGlue PATCH requests
 def patch(url,headers,payload):
 	
 	first = payload['data']['attributes']['first_name']
@@ -81,7 +85,7 @@ def patch(url,headers,payload):
 	else:
 		return response
 	
-
+# ITGlue POST requests
 def post(url,headers,payload):
 
 	first = payload['data']['attributes']['first_name']
@@ -101,117 +105,136 @@ def post(url,headers,payload):
 		return response
 	
 
-
-#Retrieve all active companies from accelo
-try:
-	active_comps = get_acc(token, f"https://{cfg.deployment}/api/v0/companies?_filters=status(3)&_limit=100")['response']
-
-except: 
-	logger("Error retrieving active companies from Accelo")
-
-#Retrive all contacts for each company from accelo
-else: 
-	eid = active_comps[13]['id']
-	organization = active_comps[13]['name']
-	print(organization)
-
+if __name__ == '__main__':
+	# Obtain token from Accelo
 	try: 
-		# Gets up to 100 contacts for each organization. Additional fields can be added.
-		contacts = get_acc(token, f'https://{cfg.deployment}/api/v0/companies/{eid}/contacts?_fields=phone, position, standing, physical_address(title)&_limit=100')['response']
-
-	except: 
-		logger(f"Error retrieving contacts for {organization}")
-
+		token = auth()
+	except:
+		logger("Could not obtain authorization token from Accelo")
 	else:
-		#Retrieve organization id from it glue based off name. Company name in Accelo must match organization in IT Glue
-
-		try: 
-			org_id = get_itg(f"https://api.itglue.com/organizations?filter[name]={organization}",itg_headers)['data'][0]['id']
-			print(f"Organization Id: {org_id}")
+		#Retrieve all active companies from accelo
+		try:
+			active_comps = get_acc(token, f"https://{cfg.deployment}/api/v0/companies?_filters=status(3)&_limit=100")['response']
 
 		except: 
-			logger(f"Could not find IT Glue organization id for {organization}")
-		
-		else:
+			logger("Error retrieving active companies from Accelo")
 
-			for each in contacts:
-				email = each['email']
+		#Retrive all contacts for each company from accelo
+		else: 
+			for comp in active_comps:
+				eid = comp['id']
+				organization = comp['name']
+				print(organization)
 
-				#Remove extra characters from phone numbers
-				phone = re.sub("[^0-9]","",each['phone'])
-				mobile = re.sub("[^0-9]","",each['mobile'])
+				try: 
+					# Gets up to 100 contacts for each organization. Additional fields can be added. For more contacts use pagination.
+					contacts = get_acc(token, f'https://{cfg.deployment}/api/v0/companies/{eid}/contacts?_fields=phone, position, standing, physical_address(title)&_limit=100')['response']
 
-				if each['standing']=='active':
-					ctype = 138082 #replace with appropriate itglue contact-type-id
-				elif each['standing']=='inactive':
-					ctype = 138081 #replace with appropriate itglue contact-type-id
+				except: 
+					logger(f"Error retrieving contacts for {organization}")
 
-			#Create json data to send to IT Glue
+				else:
+					#Retrieve organization id from it glue based off name. Company name in Accelo must match organization in IT Glue
 
-				data = {
-					"data" : {
-						"type" : "contacts",
-						"attributes" :{
-							"first_name" : f"{each['firstname']}",
-							"last_name" : f"{each['surname']}",
-							"title" : f"{each['position']}",
-							"location-name" : f"{each['physical_address']['title']}",
-							"contact-type-id" : ctype,
-							"contact-emails" : [
-								{
-									"value" :f"{email}",
-									"primary" : True,
-									"label-name" : "Email"
-								}
-							],
-							"contact-phones" : [
-								{
-									"label-name" : "Work",
-									"value" : phone
-								},
-								{
-									"label-name" : "Mobile",
-									"value" : mobile
-								}
-							]
+					try: 
+						org_id = get_itg(f"https://api.itglue.com/organizations?filter[name]={organization}",itg_headers)['data'][0]['id']
+						print(f"Organization Id: {org_id}")
 
-						}
-					}
-				}
-				print(data['data']['attributes']['first_name'], data['data']['attributes']['last_name'])
-
-				# If there is no phone or mobile info remove from list
-				
-				if not each['phone'] and not each['mobile']:
-					data['data']['attributes']['contact-phones'].pop(0)
-					data['data']['attributes']['contact-phones'].pop(0)
-				elif not each['phone']:
-					data['data']['attributes']['contact-phones'].pop(0)
-				elif not each['mobile']:
-					data['data']['attributes']['contact-phones'].pop(1)
-				
-			
-
-			# If an email exists in contact, search for it in IT Glue and retrieve contact id #
-				if email:
-					search = get_itg(f"https://api.itglue.com/contacts?filter[primary_email]={email}",itg_headers)['data']
+					except: 
+						logger(f"Could not find IT Glue organization id for {organization}")
 					
-					
-			# If email found in IT Glue, update Contact using PATCH /organizations/:organization_id/relationships/contacts/:id
-					if search:
-						contact_id = search[0]["id"]
-						print(f'id: {contact_id}')
-						r = patch(f"https://api.itglue.com/organizations/{org_id}/relationships/contacts/{contact_id}",itg_headers,data)
-						if r: 
-							print(r.text)
+					else:
 
-			# If email not found in IT Glue, create Contact using POST /organizations/:organization_id/relationships/contacts
+						for each in contacts:
+							email = each['email']
+							if each['physical_address']:
+								location = get_itg(f"https://api.itglue.com/organizations/{org_id}/relationships/locations?filter[name]={each['physical_address']['title']}",itg_headers)['data']
+								if location:
+									loc_id = location[0]['id']
+							else:
+								loc_id = None
 
-					else: 
-						print("contact not found- creating contact")
-						r = post(f"https://api.itglue.com/organizations/{org_id}/relationships/contacts",itg_headers,data)
-						if r: 
-							print(r.text)
+
+							#Remove extra characters from phone numbers
+							phone = re.sub("[^0-9]","",each['phone'])
+							mobile = re.sub("[^0-9]","",each['mobile'])
+
+							if each['standing']=='active':
+								ctype = 138082 #replace with appropriate itglue contact-type-id
+							elif each['standing']=='inactive':
+								ctype = 138081 #replace with appropriate itglue contact-type-id
+							else:
+								ctype = None
+
+							#Create json data to send to IT Glue
+
+							data = {
+								"data" : {
+									"type" : "contacts",
+									"attributes" :{
+										"first_name" : each['firstname'],
+										"last_name" : each['surname'],
+										"title" : each['position'],
+										"location-id" : loc_id,
+										"contact-type-id" : ctype,
+										"contact-emails" : [
+											{
+												"value" :email,
+												"primary" : True,
+												"label-name" : "Email"
+											}
+										],
+										"contact-phones" : [
+											{
+												"label-name" : "Work",
+												"value" : phone
+											},
+											{
+												"label-name" : "Mobile",
+												"value" : mobile
+											}
+										]
+
+									}
+								}
+							}
+							print(data['data']['attributes']['first_name'], data['data']['attributes']['last_name'])
+								
+
+						# If there is no phone or mobile info remove from contact-phones list
+								
+							if not each['phone'] and not each['mobile']:
+								data['data']['attributes']['contact-phones'].pop(0)
+								data['data']['attributes']['contact-phones'].pop(0)
+							elif not each['phone']:
+								data['data']['attributes']['contact-phones'].pop(0)
+							elif not each['mobile']:
+								data['data']['attributes']['contact-phones'].pop(1)
+								
+
+						# If an email exists in contact, search for it in IT Glue and retrieve contact id #
+							if email:
+								search = get_itg(f"https://api.itglue.com/contacts?filter[primary_email]={email}",itg_headers)['data']
+								
+									
+						# If email found in IT Glue, update Contact using PATCH /organizations/:organization_id/relationships/contacts/:id
+								if search:
+									contact_id = search[0]["id"]
+									print(f'id: {contact_id}')
+									r = patch(f"https://api.itglue.com/organizations/{org_id}/relationships/contacts/{contact_id}",itg_headers,data)
+									if r: 
+										print(r.text)
+										
+
+						# If email not found in IT Glue, create Contact using POST /organizations/:organization_id/relationships/contacts
+
+								else: 
+									print("contact not found- creating contact")
+									r = post(f"https://api.itglue.com/organizations/{org_id}/relationships/contacts",itg_headers,data)
+									if r: 
+										print(r.text)
+							
+										
 
 
 
